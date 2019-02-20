c0f750299dd34231a14c38936872bebba6bc5def
Rup Gautam <rupngautam@gmail.com>
2019-02-19 23:15:41 -0600

    sample nodejs

---
+++ sample-nodejs/app/app.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+app = express.createServer();
+
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+app.listen(80)
+console.log('Working fine!');
